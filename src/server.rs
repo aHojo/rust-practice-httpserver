444806666e0dfc859cec88d6f2480330615fe37f
ahojo <ahojodev@gmail.com>
2021-11-24 23:26:06 -0500

    Can now send data to the socket

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,4 @@
+use std::io::Read;
 // Every file is treated as a module this is like mod server{}
 use std::net::TcpListener;
 pub struct Server {
@@ -17,7 +18,30 @@ impl Server {
         println!("Listening on {}", self.addr);
 
         loop {
-            listener.accept();
+
+            match listener.accept() {
+                // because result is Result<(TcpStream, SocketAddr)> | _ is ignoring SocketAddr
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024]; // give 1024 zeroes.
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Recieved a request: {}", String::from_utf8_lossy(&buffer))
+                        },
+                        Err(e) => println!("Failed to read from the socket: {}", e),
+                    }
+
+
+                },
+                Err(e) => println!("Failed to establish a connection:  {}",  e)
+            }
+
+            // let res = listener.accept();
+            //
+            // if res.is_err() {
+            //     continue;
+            // }
+            // let (stream, address) = res.unwrap();
+
         }
     }
 }
