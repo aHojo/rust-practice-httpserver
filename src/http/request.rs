985dabe53efe1dc2fbb4921aa052579af01ded06
ahojo <ahojodev@gmail.com>
2021-11-29 16:12:25 -0500

    lots of stuff, ? operator, traits, implementations of traits, iterating over strings

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,6 +1,151 @@
-use super::method::METHOD;
+use super::method::{METHOD, MethodError}; // super means the parent module which is http here.
+use std::convert::TryFrom;
+use std::convert::From;
+use std::error::Error; // trait
+// use std::fmt::Display; // trait
+// use std::fmt::Debug; // Trait
+use std::fmt::{Result as FmtResult, Display, Debug, Formatter}; // Alias for this type.
+// use std::fmt::Formatter;
+
+use std::str;
+use std::str::Utf8Error;
+
+
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: METHOD,
+}
+
+impl Request {
+    // Result
+    fn from_byte_array(buf: &[u8]) -> Result<Self, String> {
+
+    }
+}
+
+// to implement traits - this are like interfaces in other languages
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+    // GET /search?name=abc&sort=1 HTTP/1.1
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+
+        // match str::from_utf8(buf) {
+        //     Ok(request) => {},
+        //     Err(_) => return Err(ParseError::InvalidEncoding),
+        // }
+
+        // match str::from_utf8(buf).or(Err(ParseError::InvalidEncoding)) {
+        //     Ok(request) => {},
+        //     Err(e) => return Err(e),
+        // }
+
+        // the ? below is taking place of the match above. Returns an error is it happens, otherwise we get the value
+        // let request = str::from_utf8(buf).or(Err(ParseError::InvalidEncoding))?;
+        let request = str::from_utf8(buf)?;
+        // match get_next_word(request) {
+        //     Some((method, request)) => {},
+        //     None => return Err(ParseError::InvalidRequest),
+        // }
+
+        // .ok_or Transforms the Option<T> into a [Result<T, E>], mapping Some(v) to Ok(v) and None to Err(err)
+        // Here we are overwriting the request variable.
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+
+        let method: METHOD = method.parse()?;
+
+        // let mut query_string = None;
+        // match path.find('?') {
+        //     Some(i) => {
+        //         query_string = Some(&path[i + 1..]); // we are expecting there to be a ? so +1 byte is fine here
+        //         path = &path[..i];
+        //     },
+        //     None => {}
+        // }
+        //
+        // let q = path.find('?');
+        // if q.is_some() {
+        //     let i = q.unwrap();
+        //     query_string = Some(&path[i + 1..]); // we are expecting there to be a ? so +1 byte is fine here
+        //     path = &path[..i];
+        // }
+        let mut query_string = None;
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i + 1..]); // we are expecting there to be a ? so +1 byte is fine here
+            path = &path[..i];
+        }
+        unimplemented!();
+    }
+}
+
+// Return Option so that we can return nothing if no string is passed in.
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    // Returns an iterator over the chars of a string slice. "abc d" => "a b c  d"
+    // let mut chars = request.chars();
+    // loop {
+    //     let item = chars.next();
+    //     match item {
+    //         Some(c) => {},
+    //         None => break,
+    //     }
+    // }
+
+    for (i, c) in request.chars().enumerate(){
+        if c == ' ' || c == '\r' {
+            // return Some((&request[..i], &request[i+1..])); Can't use this because if a utf8 char is more than one byte we will crash
+            return Some((&request[..i], &request[i+1..])); // same code but we know we can use this because we are
+            // ending on a space, and we know spaces are always 1 byte.
+        }
+    }
+    None
+}
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+
+
+impl ParseError {
+    fn message(&self) -> &str {
+        // The match strings are automatically returned.
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method",
+        }
+    }
+}
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+impl Error for ParseError {}
+
+// We need this here so that we can call str::from_utf8(buf)?; the ? requires this trait be implemented.
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
+    }
+}
+
+impl From<MethodError> for ParseError {
+    fn from(_: MethodError) -> Self {
+        Self::InvalidMethod
+    }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,9 @@
 use std::io::Read;
+use std::convert::TryFrom; // we need to pull in this trait to use try_from on Request.
+use std::convert::TryInto; // get this for free because of TryFrom
+use crate::http::Request; // Crate means go to the root of the crate.
+
+
 // Every file is treated as a module this is like mod server{}
 use std::net::TcpListener;
 pub struct Server {
@@ -22,10 +27,18 @@ impl Server {
             match listener.accept() {
                 // because result is Result<(TcpStream, SocketAddr)> | _ is ignoring SocketAddr
                 Ok((mut stream, _)) => {
-                    let mut buffer = [0; 1024]; // give 1024 zeroes.
+                    let mut buffer = [0; 1024]; // give 1024 zeroes and is an Array.
                     match stream.read(&mut buffer) {
                         Ok(_) => {
-                            println!("Recieved a request: {}", String::from_utf8_lossy(&buffer))
+                            println!("Recieved a request: {}", String::from_utf8_lossy(&buffer));
+                            // Request::try_from(&buffer as &[u8]); // convert to byte slice
+                            // this also converts to a slice.
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {},
+                                Err(e) => println!("Failed to parse a request: {}", e),
+                            }
+                            // let res: &Result<Request, _> = &buffer[..].try_into();
+
                         },
                         Err(e) => println!("Failed to read from the socket: {}", e),
                     }
